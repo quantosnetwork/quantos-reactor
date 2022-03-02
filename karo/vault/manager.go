b245c174f3f8b37ef4c720bc9e896565a25dd1d2
Quantos Blockchain & Network <97577935+quantosnetwork@users.noreply.github.com>
2022-03-01 19:37:52 -0500

    vault docker

--- karo/vault/manager.go
+++ karo/vault/manager.go
@@ -1,15 +1,60 @@
 package vault
 
 import (
+	"encoding/hex"
 	vaultAPI "github.com/hashicorp/vault/api"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 type Manager struct {
-	api       vaultAPI.Client
-	config    vaultAPI.Config
+	api       *vaultAPI.Client
 	tlsConfig vaultAPI.TLSConfig
+	paths     map[string]string
+}
+
+type SecretManager interface {
+	SetSecret(keyName string, key []byte) error
+	GetSecret(keyName string) ([]byte, error)
 }
 
 func (v *Manager) SetSecret(keyName string, key []byte) error {
-	return nil
+	input := map[string]interface{}{
+		"data": map[string]interface{}{
+			keyName: hex.EncodeToString(key),
+		},
+	}
+	_, err := v.api.Logical().Write("/secret/data/"+keyName, input)
+	return err
+}
+
+func (v *Manager) GetSecret(keyName string) ([]byte, error) {
+	data, err := v.api.Logical().Read("/secret/data/" + keyName)
+	if err != nil {
+		return nil, err
+	}
+	return hex.DecodeString(data.Data[keyName].(string))
+}
+
+func (v *Manager) GetRootToken() string {
+	file, _ := ioutil.ReadFile(".secret")
+	return string(file)
+}
+
+func (v *Manager) initAPI() {
+	v.api.SetToken(v.GetRootToken())
+}
+
+func NewSecretManager() SecretManager {
+	var httpClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	m := &Manager{}
+	m.api, _ = vaultAPI.NewClient(
+		&vaultAPI.Config{
+			Address: "url", HttpClient: httpClient,
+		})
+	m.initAPI()
+	return m
 }
